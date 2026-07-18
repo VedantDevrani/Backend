e8f9374b2385c20202f9726837a2ab95e6527feb
Vedant Devrani <vedantdevrani177@gamil.com>
2026-07-19 02:04:16 +0530

    express configuration

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ dotenv.config({
 
 
 connectDB()
-
-
-
-
-
-
-
-
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port : ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("MONGO DB CONNECTION FAILED : ",error)
+})
 
 
 
