b8be4c6acd0cdd5fc0b744fdf79d0d4012ae09f5
Vedant Devrani <vedantdevrani177@gamil.com>
2026-07-30 00:32:25 +0530

    user controller and user routes configuration

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try{
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\n MongoDB is connected !! Connection Host : ${connectionInstance}`)
+        console.log(`\n MongoDB is connected !! Connection Host : ${connectionInstance.connection.host}`)
     }
     catch(error){
         console.log("DB connection Error : ",error);
