56f005a02e53c1d8efa1ff714b35d35dcffb1847
Vedant Devrani <vedantdevrani177@gamil.com>
2026-08-01 18:11:00 +0530

    added user controller and big fixes

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try{
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\n MongoDB is connected !! Connection Host : ${connectionInstance.connection.host}`)
+        console.log(`\n MongoDB is connected !! Connection Host : ${connectionInstance}`)
     }
     catch(error){
         console.log("DB connection Error : ",error);
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,7 +55,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save",async function(next) {
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password,10);
+    this.password = await bcrypt.hash(this.password,10);
     next();
 })
 
@@ -91,4 +91,4 @@ userSchema.methods.generateRefreshToken = function(){
 }
 
 
-export default User = model("User",userSchema);
+export const User = model("User",userSchema);
